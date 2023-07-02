0be557a3d6b46b79f27d220824307abf138078fa
naufalmaulana <naufallmaulana@yahoo.com>
2023-07-02 23:04:35 +0700

    add cover page, footer and bio page

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+$( document ).ready(function() {
+    $('#coverButton').on("click", function(){
+        $('#cover').css("top", "-150%");
+        $('body').css("overflow", "visible");
+    })
+});
